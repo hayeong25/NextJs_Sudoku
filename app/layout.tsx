b8cab9c12fad40b694d9da6946f7210398a4af7b
hayeong <hayeong0624@gmail.com>
2025-10-11 21:41:28 +0900

    feat: add PropellerAds

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,9 @@ import * as React from "react";
 export const metadata: Metadata = {
     title: "Sudoku",
     description: "Next.js-based web page where you can enjoy Sudoku games",
+    other: {
+        'propeller': '9e39afa0361fca43d0b0455e6608584e' // PropellerAds
+    }
 };
 
 export default function RootLayout({children,}: Readonly<{
